d2f1cff725e41b86bf20799ad35d61d7ec987225
imooc <359625410@qq.com>
2018-08-02 10:07:31 +0800

    'v1.0'

---
+++ src/main/java/com/mmall/Exception/checkException.java
@@ -0,0 +1,19 @@
+package com.mmall.Exception;
+
+/**
+ * @author Jorker
+ * @date 2018/7/20 11:09
+ */
+public class checkException extends RuntimeException {
+    private Integer code;
+    public checkException(Integer code ,String msg){
+        //调用父类的构造函数的话只能在开头进行调用
+        super(msg);
+        this.code=code;
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/mmall/vo/test.java
@@ -0,0 +1,27 @@
+package com.mmall.vo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author Jorker
+ * @date 2018/8/1 16:20
+ */
+public class test {
+    static List<Integer> result=new ArrayList<>();
+    static int target=0;
+    public static void main(String[] args){
+        Scanner crc=new Scanner(System.in);
+            target=crc.nextInt();
+
+String result=crc.next();
+StringBuffer stringBuffer=new StringBuffer(result);
+String s=stringBuffer.substring(1);
+    }
+
+
+
+
+
+}
