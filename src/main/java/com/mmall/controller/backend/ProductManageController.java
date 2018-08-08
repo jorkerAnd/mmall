6c16b8e8871517b373561d80ad8715c428fef6fb
imooc <359625410@qq.com>
2018-08-08 08:37:42 +0800

    redis cache Shared

--- src/main/java/com/mmall/controller/backend/ProductManageController.java
+++ src/main/java/com/mmall/controller/backend/ProductManageController.java
@@ -10,7 +10,6 @@ import com.mmall.service.IFileService;
 import com.mmall.service.IProductService;
 import com.mmall.service.IUserService;
 import com.mmall.util.*;
-import com.sun.xml.internal.ws.resources.HttpserverMessages;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
