6d77e3bbbaf9c14c9c6f48dea0a40b38858b4b9f
imooc <359625410@qq.com>
2018-08-08 08:18:20 +0800

    spring session

---
+++ src/main/java/com/mmall/aspect/aspect.java
@@ -0,0 +1,32 @@
+package com.mmall.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+//@Aspect
+//@Component
+public class aspect {
+/**
+ * 切片设置成功
+ */
+
+    //@Pointcut("execution(public * com.mmall.controller.portal..*(..))")
+//public void pointcut(){}
+//public aspect(){
+//    System.out.println("===================begin====================");
+//}
+//
+//@Before("pointcut()")
+//    public void jorker(){
+//   log.info("jorker is coming");
+//
+//}
+
+
+
+
+}
---
+++ src/main/java/com/mmall/controller/portal/UserSpringController.java
@@ -0,0 +1,95 @@
+package com.mmall.controller.portal;
+
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisShardedPoolUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by geely
+ */
+@Controller
+@RequestMapping("/user/spring/")
+@Slf4j
+public class UserSpringController {
+
+
+    @Autowired
+    private IUserService iUserService;
+
+
+    /**
+     * 用户登录
+     *
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "login.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletResponse httpServletResponse) {
+        ServerResponse<User> response = iUserService.login(username, password);
+        if (response.isSuccess()) {
+            session.setAttribute(Const.CURRENT_USER, response.getData());
+
+/**
+ * EB60894CC11E1574B1118A975BF17053
+ * JSESSIONID=EB60894CC11E1574B1118A975BF17053
+ * 一个浏览器访问一次就会出现了一次独有的session
+ *
+ */
+
+//
+//            CookieUtil.writeLoginToken(httpServletResponse, session.getId());
+//            //CookieUtil.readLoginToken(httpServletRequest);
+//            //CookieUtil.delLoginToken(httpServletRequest,httpServletResponse);
+//            RedisShardedPoolUtil.setEx(session.getId(), JsonUtil.obj2String(response.getData()), Const.RedisCacheExtime.REDIS_SEESION_EXTIME);
+
+
+        }
+        return response;
+    }
+
+    @RequestMapping(value = "logout.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, HttpSession httpSession) {
+//        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+//        CookieUtil.delLoginToken(httpServletRequest, httpServletResponse);
+//        RedisShardedPoolUtil.del(loginToken);
+//        String result=CookieUtil.readLoginToken(httpServletRequest);
+//
+//        log.info(result);
+        httpSession.removeAttribute(Const.CURRENT_USER);
+        return ServerResponse.createBySuccess();
+    }
+
+    @RequestMapping(value = "get_user_info.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> getUserInfo(HttpSession httpSession) {
+
+
+        User user = (User) httpSession.getAttribute(Const.CURRENT_USER);
+        if (user != null)
+            return ServerResponse.createBySuccess(user);
+        return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
+
+    }
+
+
+}
